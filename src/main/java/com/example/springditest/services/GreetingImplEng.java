139b8b6a0dae48e771132553d86d9678fbe48abd
sugan0tech <sugankpms@gmail.com>
2022-09-26 18:59:02 +0530

    added profile

--- src/main/java/com/example/springditest/services/GreetingImplEng.java
+++ src/main/java/com/example/springditest/services/GreetingImplEng.java
@@ -1,6 +1,5 @@
 package com.example.springditest.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 

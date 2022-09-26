be205e3431189597be2c41b72f6e0d29249ce6fa
sugan0tech <sugankpms@gmail.com>
2022-09-26 18:43:04 +0530

    initial commit

---
+++ src/main/java/com/example/springditest/SpringDiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springditest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springditest/SpringDiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springditest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
